d9b614ed7e94eb45df878f1116e9169de3f5e409
EmileCar <emile.caron@student.vives.be>
2024-06-18 05:51:00 +0200

    environments

--- api/database/connection.php
+++ api/database/connection.php
@@ -1,22 +1,26 @@
 <?php
 
-
 use Illuminate\Database\Capsule\Manager as Capsule;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(dirname(__DIR__, 2));
+$dotenv->load();
+
 $capsule = new Capsule;
 $capsule->addConnection([
-'driver'    => 'mysql',
-'host'      => 'prive',
-'database'  => 'prive',
-'username'  => 'prive',
-'password'  => 'prive',
-'charset'   => 'utf8mb4',
-'collation' => 'utf8mb4_unicode_ci',
-'prefix'    => '',
+    'driver' => 'mysql',
+    'host' => $_ENV['DB_HOST'],
+    'database' => $_ENV['DB_DATABASE'],
+    'username' => $_ENV['DB_USERNAME'],
+    'password' => $_ENV['DB_PASSWORD'],
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
 ]);
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
 
-try{
+try {
     $capsule->getConnection()->getPdo();
 } catch (Exception $e) {
     http_response_code(500);
