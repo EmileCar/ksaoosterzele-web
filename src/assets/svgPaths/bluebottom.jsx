75d226550fd5634ba665d4fb5621049e4a01c0cd
EmileCar <emile.caron@student.vives.be>
2024-06-18 06:19:31 +0200

    added assets and dependancies

---
+++ src/assets/svgPaths/bluebottom.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function BlueBottom() {
+
+    const fillStyle = {
+        fill: "#fff"
+    };
+
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1240.84 53.05" className="down secondSVG">
+            <g id="b">
+              <g id="c">
+                <path className="d" d="M0,0H1240.84s-289.44,53.05-620.42,53.05S0,0,0,0Z" />
+              </g>
+            </g>
+        </svg>
+    );
+}
+
+export default BlueBottom;
---
+++ src/assets/svgPaths/bluetop.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function BlueTop() {
+
+    const fillStyle = {
+        fill: "#fff"
+    };
+
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1240.84 53.05" style={{top: "1px"}} className='top '>
+            <g id="b">
+                <g id="c">
+                    <path className="d" d="M1240.84,53.05H0S289.44,0,620.42,0s620.42,53.05,620.42,53.05Z" />
+                </g>
+            </g>
+        </svg>
+    );
+}
+
+export default BlueTop;
