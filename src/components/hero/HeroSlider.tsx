b76adfd2855e7b0d3dc0585760ab3a27699fc618
EmileCar <emile.caron@telenet.be>
2024-06-19 18:40:17 +0200

    blurred hero image

--- src/components/hero/HeroSlider.tsx
+++ src/components/hero/HeroSlider.tsx
@@ -1,10 +1,18 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from "react";
 
 const HeroSlider = () => {
   const [activeImageIndex, setActiveImageIndex] = useState<number>(0);
-  const imageNames = ['main', 'second', 'third', 'fourth'];
+  const imageNames = useMemo(() => [
+    "stellingstartdag",
+    "etenopkamp",
+    "vierkantsformatie",
+    "kampsfeer",
+  ], []);
+  const [loadedImages, setLoadedImages] = useState<boolean[]>(Array(imageNames.length).fill(false));
+  const [allImagesLoaded, setAllImagesLoaded] = useState<boolean>(false);
 
   useEffect(() => {
+    if(!allImagesLoaded) return;
     const interval = setInterval(() => {
       setActiveImageIndex((prevIndex) => (prevIndex + 1) % imageNames.length);
     }, 7000);
@@ -12,17 +20,53 @@ const HeroSlider = () => {
     return () => {
       clearInterval(interval);
     };
-  }, [imageNames.length]);
+  }, [imageNames.length, allImagesLoaded]);
+
+  useEffect(() => {
+    const loadImages = () => {
+      imageNames.forEach((imageName, index) => {
+        const img = new Image();
+        img.src = `../../assets/hero/${imageName}.jpg`;
+        img.onload = () => {
+          setLoadedImages((prev) => {
+            const updated = [...prev];
+            updated[index] = true;
+            if (updated.every((status) => status)) {
+              setAllImagesLoaded(true);
+            }
+            return updated;
+          });
+        };
+      });
+    };
+
+    loadImages();
+  }, [imageNames]);
 
   return (
     <div className="hero__images--container layered-grid">
-      {imageNames.map((image, index) => (
+      {imageNames.map((imageName, index) => (
         <div
-          key={image}
-          className={`hero__image ${image} ${index === activeImageIndex ? 'active' : ''}`}
-        ></div>
+          key={imageName}
+          className={`hero__image ${
+            index === activeImageIndex ? "active" : ""
+          }`}
+        >
+          <img
+            src={`../../assets/hero/${imageName}.jpg`}
+            alt={`KSA Oosterzele ${imageName}`}
+            style={{ display: loadedImages[index] ? "block" : "none" }}
+          />
+        </div>
       ))}
-      <div className="hero__images--foreground"></div>
+      {!allImagesLoaded && (
+        <div className="hero__image active">
+          <img
+            src={`../../assets/hero/blur.jpg`}
+            alt="KSA Oosterzele blurred image"
+          />
+        </div>
+      )}
     </div>
   );
 };
