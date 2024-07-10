f043ac40e9393a1ce4f61b1ade9376a113e1cb5f
Emile Caron <caron.emile@telenet.be>
2024-07-10 13:32:45 +0200

    imageList

--- src/components/collages/ImageList.tsx
+++ src/components/collages/ImageList.tsx
@@ -1,34 +1,33 @@
-import React, { useCallback, useEffect, useState } from "react";
+import { useState } from "react";
 import "./ImageList.css";
-import { deleteImageOfCollage } from "../../services/mediaService";
-import useFetch from "../../hooks/useFetch";
 import Collage from "../../types/Collage";
-import FetchedDataLayout from "../../layouts/FetchedDataLayout";
-import Default from '../../assets/img/default.jpg';
+import defaultThumbnail from '../../assets/img/default.jpg';
 
-const ImageList = ({ collage, images } : { collage: Collage, images: string[] }) => {
+const ImageList = ({ collage, images, isAdmin = false } : { collage: Collage, images: string[], isAdmin?: boolean }) => {
     const [detail, setDetail] = useState<string | null>()
 
     const deleteImage = async (imageName: string) => {
-        
+
     };
 
     return (
-        <ul className="image-list">
-            {images.map((image) => (
-                <img
-                    key={image}
-                    src={`../../assets/media/collages/${collage.name}/thumbnails/${image}`}
-                    alt={collage.displayName + " foto"}
-                    width={200}
-                    height={150}
-                    onError={(e) => {
-                        e.currentTarget.src = Default;
-                    }}
-                    className="image"
-                    onClick={() => setDetail(image)}
-                />
-            ))}
+        <>
+            <ul className="image-list">
+                {collage.images && collage.images.map((image) => (
+                    <img
+                        key={image}
+                        src={`../../assets/media/collages/${collage.name}/thumbnails/${image}`}
+                        alt={collage.displayName + " foto"}
+                        width={200}
+                        height={150}
+                        onError={(e) => {
+                            (e.target as HTMLImageElement).src = defaultThumbnail;
+                        }}
+                        className="image"
+                        onClick={() => setDetail(image)}
+                    />
+                ))}
+            </ul>
             {detail && (
                 <div className="overlay overlay-media" onClick={() => setDetail(null)}>
                     <span className="close-media" onClick={() => setDetail(null)}>
@@ -38,13 +37,13 @@ const ImageList = ({ collage, images } : { collage: Collage, images: string[] })
                         src={`../../assets/media/collages/${collage.name}/${detail}`}
                         alt={collage.displayName + " foto"}
                         onError={(e) => {
-                            e.currentTarget.src = Default;
+                            (e.target as HTMLImageElement).src = defaultThumbnail;
                         }}
                         className="detail__image"
                     />
                 </div>
             )}
-        </ul>
+        </>
 )}
 
 export default ImageList;
--- src/pages/media/CollageDetail.tsx
+++ src/pages/media/CollageDetail.tsx
@@ -1,12 +1,11 @@
 import { useCallback, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { getCollage } from "../../services/mediaService";
-import "./Media.css";
 import useFetch from "../../hooks/useFetch";
 import PageLayout from "../../layouts/PageLayout";
 import FetchedDataLayout from "../../layouts/FetchedDataLayout";
 import SectionTitle from "../../components/sectionTitle/SectionTitle";
-import defaultThumbnail from '../../assets/img/default.jpg';
+import ImageList from "../../components/collages/ImageList";
 
 const CollageDetail = () => {
     const { id } = useParams();
@@ -26,38 +25,8 @@ const CollageDetail = () => {
                             <p>Bekijk hier de fotos en videos van {collage.displayName ?? "deze collage"}</p>
                         </SectionTitle>
                         <div className="page__section--content">
-                            <ul className="image-list">
-                            {collage.images && collage.images.map((image) => (
-                                <img
-                                    key={image}
-                                    src={`../../assets/media/collages/${collage.name}/thumbnails/${image}`}
-                                    alt={collage.displayName + " foto"}
-                                    width={200}
-                                    height={150}
-                                    onError={(e) => {
-                                        (e.target as HTMLImageElement).src = defaultThumbnail;
-                                    }}
-                                    className="image"
-                                    onClick={() => setDetail(image)}
-                                />
-                            ))}
-                            </ul>
+                            <ImageList collage={collage} images={collage.images ?? []} />
                         </div>
-                        {detail && (
-                            <div className="overlay overlay-media" onClick={() => setDetail(null)}>
-                                <span className="close-media" onClick={() => setDetail(null)}>
-                                    &times;
-                                </span>
-                                <img
-                                    src={`../../assets/media/collages/${collage.name}/${detail}`}
-                                    alt={collage.displayName + " foto"}
-                                    onError={(e) => {
-                                        (e.target as HTMLImageElement).src = defaultThumbnail;
-                                    }}
-                                    className="detail__image"
-                                />
-                            </div>
-                        )}
                     </>
                 )}
             </FetchedDataLayout>
